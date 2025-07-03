b2714aa6120ddc3476fd20d01a7be2cdd87b93ef
Kosmos-git <leobog18@yandex.ru>
2025-07-03 17:15:49 +0300

    HW3

--- Program.cs
+++ Program.cs
@@ -4,27 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace lessonOne
+namespace homeworkTwo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int level = 10;
-            int numberStudents = 127;
-            uint healthpointBigBoss = 1000000000;
-
-            float priceBarrelOil = 68.94f;
-            double eccentricityEarthOrbit = 0.0167;
-
-            bool isOpen = false;
-            bool isGameWon = true;
-            bool haveCompletedLevel = true;
-
-            char currency = '$';
-
-            string nameRestaurant = "Space";
-            string welcomeSpeech = "Дорогие программисты, мы начинаем!!";
+            string name, age, placeOfBirth, profession, hobby, favoriteSchoolSubject, purposeInLife;
+            Console.WriteLine("Здравствуйте! Вы приглашены в гости в Урганту! Перед передачей необходимо ответить на несколько вопросов. Давайте начнем.");
+            Console.Write("Как Вас зовут? ");
+            name = Console.ReadLine();
+            Console.Write("Сколько Вам лет? ");
+            age = Console.ReadLine();
+            Console.Write("Где Вы родились? ");
+            placeOfBirth = Console.ReadLine();
+            Console.Write("Кто Вы по профессии? ");
+            profession = Console.ReadLine();
+            Console.Write("Какое у Вас хобби? ");
+            hobby = Console.ReadLine();
+            Console.Write("Какой предмет в школе был у вас любимым? ");
+            favoriteSchoolSubject = Console.ReadLine();
+            Console.Write("Какая у Вас цель в жизни? ");
+            purposeInLife = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"Вас зовут {name}, Вам {age}, Вы родились {placeOfBirth}, по профессии Вы {profession}, ваше хобби {hobby}, " +
+                $"в школе Ваш любимый предмет был {favoriteSchoolSubject}, ваша цель в жизни {purposeInLife}.");
         }
     }
 }
