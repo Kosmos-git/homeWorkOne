cd96248df1065ca248663bfa275fc48b6595b532
Kosmos-git <leobog18@yandex.ru>
2025-07-03 16:01:49 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessonOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = 10;
+            int numberStudents = 127;
+            uint healthpointBigBoss = 1000000000;
+
+            float priceBarrelOil = 68.94f;
+            double eccentricityEarthOrbit = 0.0167;
+
+            bool isOpen = false;
+            bool isGameWon = true;
+            bool haveCompletedLevel = true;
+
+            char currency = '$';
+
+            string nameRestaurant = "Space";
+            string welcomeSpeech = "Дорогие программисты, мы начинаем!!";
+        }
+    }
+}
