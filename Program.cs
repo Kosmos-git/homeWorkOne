863c2353ba80768aa5a16575333ece4f14432e52
Kosmos-git <leobog18@yandex.ru>
2025-07-03 23:11:41 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hw6
+namespace hw7
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            int priceCrystal = 25;
-            Console.WriteLine($"Добро пожаловать в магазин кристаллов. Стоимость кристалла {priceCrystal} монет.");          
+        {       
+            int timeOneAppointment = 10;
+            int minutesInHour = 60;
 
-            Console.Write("Сколько у Вас монет? ");
-            int money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько людей в очереди? ");
+            int peopleInQueue = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Сколько кристаллов Вы хотите приобрести? ");
-            int crystals = Convert.ToInt32(Console.ReadLine());
+            int timeInQueue = peopleInQueue * timeOneAppointment;
 
-            money -= crystals * priceCrystal;
-            Console.WriteLine($"У Вас в рюкзаке {crystals} кристаллов и {money} монет.");
+            int hoursInQueue = timeInQueue / minutesInHour;
+            int minutesInQueue = timeInQueue % minutesInHour;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursInQueue} часов и {minutesInQueue} минут.");
         }
     }
 }
