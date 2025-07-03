ea5cab4f821571a48e150d31a661fb77c020ff27
Kosmos-git <leobog18@yandex.ru>
2025-07-03 22:42:33 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hw5
+namespace hw6
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string name = "Ivanov";
-            string surname = "Leonid";
-            string buffer;
+            int priceCrystal = 25;
+            Console.WriteLine($"Добро пожаловать в магазин кристаллов. Стоимость кристалла {priceCrystal} монет.");          
 
-            Console.WriteLine($"Исходное значение переменных:\nname = {name}\nsurname = {surname}");
+            Console.Write("Сколько у Вас монет? ");
+            int money = Convert.ToInt32(Console.ReadLine());
 
-            buffer = name;
-            name = surname;
-            surname = buffer;
+            Console.Write("Сколько кристаллов Вы хотите приобрести? ");
+            int crystals = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Значение переменных после перестановки:\nname = {name}\nsurname = {surname}");
+            money -= crystals * priceCrystal;
+            Console.WriteLine($"У Вас в рюкзаке {crystals} кристаллов и {money} монет.");
         }
     }
 }
