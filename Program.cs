13b8f91dc89da470d1781e10ece91612b784a11e
Kosmos-git <leobog18@yandex.ru>
2025-07-03 21:50:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,38 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace homeworkTwo
+namespace hw4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.Unicode;
-            Console.OutputEncoding = Encoding.Unicode;
+            int pictures = 52;
+            int picturesInRow = 3;
+            int rowsFull;
+            int picturesUnused;
 
-            Console.WriteLine("Здравствуйте! Вы приглашены в гости к Урганту! Перед передачей необходимо ответить на несколько вопросов. Давайте начнем.");
+            rowsFull = pictures / picturesInRow;
+            picturesUnused = pictures % picturesInRow;
 
-            Console.Write("Как Вас зовут? ");
-            string name = Console.ReadLine();
-
-            Console.Write("Сколько Вам лет? ");
-            string age = Console.ReadLine();
-            
-            Console.Write("Кто Вы по профессии? ");
-            string profession = Console.ReadLine();
-            
-            Console.Write("Какое у Вас хобби? ");
-            string hobby = Console.ReadLine();
-            
-            Console.Write("Какой предмет в школе был у вас любимым? ");
-            string favoriteSchoolSubject = Console.ReadLine();
-            
-            Console.Write("Какая у Вас цель в жизни? ");
-            string purposeInLife = Console.ReadLine();
-            
-            Console.WriteLine();
-            Console.WriteLine($"Вас зовут {name}.\nВам {age}.\nВаше хобби: {hobby}.\n" +
-                $"Ваш любимый предмет в школе: {favoriteSchoolSubject}.\nВаша цель в жизни: {purposeInLife}.");
+            Console.WriteLine($"Полностью заполненных рядов будет выведено: {rowsFull}");
+            Console.WriteLine($"Картинок сверх меры будет: {picturesUnused}");
         }
     }
 }
