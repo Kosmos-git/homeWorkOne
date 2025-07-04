979bcff50c23bbbc86deff1485b375fe0edd3436
Kosmos-git <leobog18@yandex.ru>
2025-07-04 10:46:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace hw7
 {
@@ -10,16 +6,16 @@ namespace hw7
     {
         static void Main(string[] args)
         {       
-            int timeOneAppointment = 10;
+            int appointmentDurationMinutes = 10;
             int minutesInHour = 60;
 
             Console.Write("Сколько людей в очереди? ");
             int peopleInQueue = Convert.ToInt32(Console.ReadLine());
 
-            int timeInQueue = peopleInQueue * timeOneAppointment;
+            int queueTimeInMinutetes = peopleInQueue * appointmentDurationMinutes;
 
-            int hoursInQueue = timeInQueue / minutesInHour;
-            int minutesInQueue = timeInQueue % minutesInHour;
+            int hoursInQueue = queueTimeInMinutetes / minutesInHour;
+            int minutesInQueue = queueTimeInMinutetes % minutesInHour;
 
             Console.WriteLine($"Вы должны отстоять в очереди {hoursInQueue} часов и {minutesInQueue} минут.");
         }
