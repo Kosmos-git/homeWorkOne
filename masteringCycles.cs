c72b3f99d81588dd56021207f9c3cf59a63615f8
Kosmos-git <leobog18@yandex.ru>
2025-07-07 22:27:43 +0300

    Add files via upload

---
+++ masteringCycles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace masteringCycles
+{
+    internal class MasteringCycles
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Введите сообщение: ");
+            string messageUser = Console.ReadLine();
+
+            Console.Write("Введите количество циклов: ");
+            int numberRepetitionsCycle = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberRepetitionsCycle; i++)
+            {
+                Console.WriteLine(messageUser);
+            }
+        }
+    }
+}
