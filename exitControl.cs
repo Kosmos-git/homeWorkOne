d52754474ebcbaa70c6d32b5e24b34b461c4bf3c
Kosmos-git <leobog18@yandex.ru>
2025-07-07 22:42:57 +0300

    Add files via upload

---
+++ exitControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exitControl
+{
+    internal class exitControl
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Программа позволяет ввести любое сообщение. Для завершения введите: 'exit'.\n\n");
+
+            string massageUser;
+            string exitProgram = "exit";
+
+            while (true)
+            {
+                Console.Write("Введите желаемое сообщение: ");
+                massageUser = Console.ReadLine();
+
+                if (massageUser == exitProgram)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("\nПрограмма заввершена.\n");
+        }
+    }
+}
