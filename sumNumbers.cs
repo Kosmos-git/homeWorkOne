a1e5faeaea50afa23f2254a89b7a579a9a163fab
Kosmos-git <leobog18@yandex.ru>
2025-07-07 23:21:30 +0300

    Add files via upload

---
+++ sumNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sumNumbers
+{
+    internal class sumNumbers
+    {
+        static void Main(string[] args)
+        {
+            int sumNumbers = 0;
+
+            Random random = new Random();
+
+            int number = random.Next(1, 101);
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sumNumbers += i;
+                }
+            }
+
+            Console.WriteLine($"Получено рандомное число: {number}.");
+            Console.WriteLine($"Сумма всех положительных чисел меньше {number}, которые кратны 3 и 5 равна {sumNumbers}.");
+        }
+    }
+}
