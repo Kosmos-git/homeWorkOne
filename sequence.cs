b5e50176e58c1a8819ebe9cf5fc9a7364ac1587b
Kosmos-git <leobog18@yandex.ru>
2025-07-07 23:16:54 +0300

    Add files via upload

---
+++ sequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace sequence
+{
+    internal class sequence
+    {
+        static void Main(string[] args)
+        {
+            int initialTermProgression = 5;
+            int differenceInProgression = 7;
+            int memberOfProgression = initialTermProgression;
+            int numberIterations;
+
+            Console.Write("Введите количество членов прогрессии, которые Вас интересуют: ");
+            numberIterations = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < numberIterations; i++)
+            {
+                Console.Write(memberOfProgression + " ");
+                memberOfProgression += differenceInProgression;
+            }
+        }
+    }
+}
