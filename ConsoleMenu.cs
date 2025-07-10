ee815050aaf3095eef2b070c4e2bce6aec796878
Kosmos-git <leobog18@yandex.ru>
2025-07-10 21:48:25 +0300

    Add files via upload

--- ConsoleMenu.cs
+++ ConsoleMenu.cs
@@ -6,36 +6,44 @@ namespace ConsoleMenu
     {
         static void Main(string[] args)
         {
-            bool exitProgram = true;
+            const string CommandAnecdoteOne = "Anecdote 1";
+            const string CommandAnecdoteTwo = "Anecdote 2";
+            const string CommandRandom = "Random";
+            const string CommandClear = "Clear";
+            const string CommandListOfPgorgam = "Commands";
+            const string CommandExit = "Exit";
+
+            bool isWork = true;
             Random random = new Random();
+            string listOfPrograms = $"Программа умеет выполнять следующие команды:\n\n" +
+                $"{CommandAnecdoteOne} - Рассказать анекдот №1.\n\n" +
+                $"{CommandAnecdoteTwo} - Рассказать анекдот №2.\n\n" +
+                $"{CommandRandom} - Вывести случайное число.\n\n" +
+                $"{CommandClear} - Очистить консоль.\n\n" +
+                $"{CommandListOfPgorgam} - Повторить список команд, которые знает программа.\n\n" +
+                $"{CommandExit} - Выход из программы.\n\n\n";
 
             Console.SetCursorPosition(0, 2);
             Console.WriteLine("Привет! Это моя первая программа. Она называется: 'Консольное меню'\n");
 
-            Console.WriteLine("Программа умеет выполнять следующие команды:\n\n" +
-                "1 - Рассказать анекдот №1.\n\n" +
-                "2 - Рассказать анекдот №2.\n\n" +
-                "3 - Вывести случайное число.\n\n" +
-                "4 - Очистить консоль.\n\n" +
-                "5 - Повторить список команд, которые знает программа.\n\n" +
-                "6 - Выход из программы.\n\n\n");
+            Console.WriteLine(listOfPrograms);
 
-            while (exitProgram)
+            while (isWork)
             {
                 Console.Write("Введите команду, которую хотите выполнить: ");
-                string commandNumber = Console.ReadLine();
+                string userInput = Console.ReadLine();
 
-                switch (commandNumber)
+                switch (userInput)
                 {
-                    case "1":
+                    case CommandAnecdoteOne:
                         Console.WriteLine("\nПорой ты пытаешься показать свой богатый внутренний мир тому, у кого внутри посёлок городского типа.\n\n");
                         break;
 
-                    case "2":
+                    case CommandAnecdoteTwo:
                         Console.WriteLine("\nПерейдём к действительно важным вопросам. Почему у Губки Боба нет зелёной шершавой штуки на спине?\n\n");
                         break;
 
-                    case "3":
+                    case CommandRandom:
                         Console.Write("\nВведите нижнюю границу для случайного числа: ");
                         int minRandomNumber = Convert.ToInt32(Console.ReadLine());
 
@@ -47,23 +55,17 @@ namespace ConsoleMenu
                         Console.WriteLine($"\nВаше случайное число: {randomNumber}\n\n");
                         break;
 
-                    case "4":
+                    case CommandClear:
                         Console.Clear();
                         break;
 
-                    case "5":
-                        Console.WriteLine("\n\nНапоминаю список команд, которые программа умеет выполнять:\n\n" +
-                            "1 - Рассказать анекдот №1.\n\n" +
-                            "2 - Рассказать анекдот №2.\n\n" +
-                            "3 - Вывести случайное число.\n\n" +
-                            "4 - Очистить консоль.\n\n" +
-                            "5 - Повторить список команд, которые знает программа.\n\n" +
-                            "6 - Выход из программы.\n\n\n");
+                    case CommandListOfPgorgam:
+                        Console.WriteLine("\n" + listOfPrograms);
                         break;
 
-                    case "6":
-                        exitProgram = false;
-                        
+                    case CommandExit:
+                        isWork = false;
+
                         Console.WriteLine("\nЗавершение программы . . .");
                         break;
 
