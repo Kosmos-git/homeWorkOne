a335301fcf9061ecb8bcc9176cc2248a9132a0af
Kosmos-git <leobog18@yandex.ru>
2025-07-14 23:01:08 +0300

    Add files via upload

--- MultiplesOfNumbers/Program.cs
+++ MultiplesOfNumbers/Program.cs
@@ -7,23 +7,25 @@ namespace MultiplesOfNumbers
         static void Main(string[] args)
         {
             Random random = new Random();
-            
-            int N = random.Next(10, 26);
+
+            int minDivider = 10;
+            int maxDivider = 25 + 1;
+            int divider = random.Next(minDivider, maxDivider);
             int minValue = 50;
             int maxValue = 150;
             int multiplesNumbers = 0;
 
-            Console.WriteLine("Дано N равное: " + N);
+            Console.WriteLine("Дано N равное: " + divider);
 
-            for (int i = N; i <= maxValue; i += N)
+            for (int i = 0; i <= maxValue; i += divider)
             {
-                if (i >= minValue && i <= maxValue)
+                if (i >= minValue)
                 {
                     multiplesNumbers++;
                 }
             }
 
-            Console.WriteLine($"Количество чисел от {minValue} до {maxValue}, которые кратны {N}: {multiplesNumbers}");
+            Console.WriteLine($"Количество чисел от {minValue} до {maxValue}, которые кратны {divider}: {multiplesNumbers}");
         }
     }
 }
