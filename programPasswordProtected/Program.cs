413535b296460171d0942f09c3dc65b068ddf976
Kosmos-git <leobog18@yandex.ru>
2025-07-14 22:32:46 +0300

    Add files via upload

---
+++ programPasswordProtected/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace programPasswordProtected
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "12348765";
+            string passwordInput;
+            int numberAttempts = 3;
+            string secretMessage = "Коды запуска ракет";
+
+            for (int i = 0; i < numberAttempts; i++)
+            {
+                Console.Write("Введите пароль: ");
+                passwordInput = Console.ReadLine();
+
+                if (passwordInput == password)
+                {
+                    Console.WriteLine(secretMessage);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Пароль не верный. Попыток осталось: {numberAttempts - i - 1}");
+                }
+            }
+        }
+    }
+}
