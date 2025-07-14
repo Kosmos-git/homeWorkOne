2a6c8f5c4aec7240afc541237a8e67b011a8609c
Kosmos-git <leobog18@yandex.ru>
2025-07-14 23:22:17 +0300

    Add files via upload

---
+++ powerOfTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace powerOfTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberInput = random.Next(100);
+            int minPowerTwo = 0;
+            int numberPowerTwo = 2;
+
+            for (int i = numberPowerTwo; i < numberInput; i *= 2)
+            {
+                minPowerTwo++;
+                numberPowerTwo = i;
+            }
+
+            minPowerTwo++;
+            numberPowerTwo *= 2;
+
+            Console.WriteLine($"Для числа {numberInput}, " +
+                $"минимальная степень двойки, превышающая само число будет: {minPowerTwo}." +
+                $"\n2 в степени {minPowerTwo}: {numberPowerTwo}");
+        }
+    }
+}
