c2cee7b0f349b9cc1bb32cdc6a2b4455af1d2828
Kosmos-git <leobog18@yandex.ru>
2025-07-14 22:49:47 +0300

    Add files via upload

---
+++ MultiplesOfNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MultiplesOfNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            int N = random.Next(10, 26);
+            int minValue = 50;
+            int maxValue = 150;
+            int multiplesNumbers = 0;
+
+            Console.WriteLine("Дано N равное: " + N);
+
+            for (int i = N; i <= maxValue; i += N)
+            {
+                if (i >= minValue && i <= maxValue)
+                {
+                    multiplesNumbers++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел от {minValue} до {maxValue}, которые кратны {N}: {multiplesNumbers}");
+        }
+    }
+}
