602946e3858d503664b139cdc321fab9d8ab0773
Kosmos-git <leobog18@yandex.ru>
2025-07-20 20:40:32 +0300

    Add files via upload

---
+++ workingWithSpecificRowsColumns/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace workingWithSpecificRowsColumns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 6, 7, 8 }
+            };
+
+            int sumSecondRow = 0;
+            int lineSecond = 1;
+
+            int productFirstColumn = 1;
+            int columnFirst = 0;
+
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                sumSecondRow += array[lineSecond, j];
+            }
+
+            Console.WriteLine("Сумма второй строки: " + sumSecondRow);
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                productFirstColumn *= array[i, columnFirst];
+            }
+
+            Console.WriteLine("Произведение первого столбца: " + productFirstColumn);
+        }
+    }
+}
