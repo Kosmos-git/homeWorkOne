0124f795d20d443152988ef792dbc2e85a1091af
Kosmos-git <leobog18@yandex.ru>
2025-07-21 14:28:10 +0300

    Add files via upload

--- localMaxima/Program.cs
+++ localMaxima/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace localMaxima
 {
@@ -6,37 +7,38 @@ namespace localMaxima
     {
         static void Main(string[] args)
         {
-            int lengthaSequenceNumbers = 30;
-            int[] sequenceNumbers = new int[lengthaSequenceNumbers];
+            int lengthaNumbers = 30;
+            int[] numbers = new int[lengthaNumbers];
             int minValue = 10;
             int maxValue = 99;
+            int indexLastNumber = numbers.Length - 1;
 
             Random random = new Random();
 
-            for (int i = 0; i < sequenceNumbers.Length; i++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                sequenceNumbers[i] = random.Next(minValue, maxValue + 1);
-                Console.Write(sequenceNumbers[i] + " ");
+                numbers[i] = random.Next(minValue, maxValue + 1);
+                Console.Write(numbers[i] + " ");
             }
 
             Console.Write("\nЛокальные максимумы: ");
 
-            if (sequenceNumbers[0] > sequenceNumbers[1])
+            if (numbers[0] > numbers[1])
             {
-                Console.Write(sequenceNumbers[0] + " ");
+                Console.Write(numbers[0] + " ");
             }
 
-            for (int i = 1; i < sequenceNumbers.Length - 1; i++)
+            for (int i = 1; i < numbers.Length - 1; i++)
             {            
-                if (sequenceNumbers[i] > sequenceNumbers[i - 1] && sequenceNumbers[i] > sequenceNumbers[i + 1])
+                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
                 {
-                    Console.Write(sequenceNumbers[i] + " ");
+                    Console.Write(numbers[i] + " ");
                 }
             }
 
-            if (sequenceNumbers[sequenceNumbers.Length - 1] > sequenceNumbers[sequenceNumbers.Length - 2])
+            if (numbers[numbers.Length - 1] > numbers[indexLastNumber - 1])
             {
-                Console.Write(sequenceNumbers[sequenceNumbers.Length - 1] + " ");
+                Console.Write(numbers[numbers.Length - 1] + " ");
             }
         }
     }
